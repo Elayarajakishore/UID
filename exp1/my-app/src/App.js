354a4f5dacbc88b8dd081cc9e6b4aeecbff0ef8a
kishore E <kishoree708@gmail.com>
2024-11-15 23:14:00 +0530

    Commited

---
+++ exp1/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import A from 'component./A'
+
+
+function App() {
+  return (
+    <div>
+      <>
+        <A />
+      
+      </>
+    </div>
+  );
+}
+
+export default App;
